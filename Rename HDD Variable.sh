990c801b608f6005a91e19bcb03a6ed75cd8ecd3
C7921 <c.ellemsanders@gmail.com>
2017-10-16 15:39:43 +1100

    Minor Format and syntax change

--- Rename HDD Variable.sh
+++ Rename HDD Variable.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+#
+#
+# Connor Sanders October 2017 
+#########################################
+#
+# Script allows the "Macintosh HD" Partitio to be renamed to another value, if needed.
+# This can be usful for data tranfers that use similar or the same paths for the  source and 
+# destination.
+# 
+# Values can be hardcoded, however this is designed to be set by parameter 4 in the JSS Policy.
+# 
+
 
 NewName="$4"
 
@@ -18,3 +30,5 @@ node=$(diskutil info / | grep "Node" | cut -c 30-)
 	echo $node 
  sudo diskutil rename $node "$NewName"
 fi
+
+exit 0
