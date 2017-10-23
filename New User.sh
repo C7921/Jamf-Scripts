953a6eacd5aa04347a31271fdb66130d76b71e44
C7921 <c.ellemsanders@gmail.com>
2017-10-23 13:41:57 +1100

    Version 1 of New User Script
    Creates new user with UniqueID with option of admin rights or not.

---
+++ New User.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+# Connor Sanders October 2017
+# Non - Jamf Version, User Prompts 
+# Items can be hardcode. However, the script is designed to run with user/admin input to create user account from those items
+# Usershell will be /bin/bash and Home Directory will be /Local/Users/Username. 
+
+# Requires Root ## Comment out if not needed
+#if [[ $UID -ne 0 ]]; then echo "Please run $0 as root." && exit 1; fi
+
+maxid=$(dscl . -list /Users UniqueID | awk '{print $2}' | sort -ug | tail -1)
+newid=$((maxid+1))
+
+#### Collecting User Details ####
+
+read -p 'Username: ' usernamevar
+
+read -p 'FullName: ' fullnamevar
+
+read -p 'Password: ' passvar
+
+# read -p 'UniqueID: ' NextID
+
+
+### Choice for admin account ###
+read -p "Will User be admin? " adminvar
+
+if [ "$adminvar" == "y" ]
+then
+    echo "Creating account with Admin Rights..."
+    sudo dscl . -create /Users/$usernamevar
+    sudo dscl . -create /Users/$usernamevar UserShell /bin/bash
+    sudo dscl . -create /Users/$usernamevar RealName $fullnamevar
+    sudo dscl . -create /Users/$usernamevar PrimaryGroupID 80
+    sudo dscl . -create /Users/$usernamevar UniqueID $newid
+    sudo dscl . -create /Users/$usernamevar NFSHomeDirectory /Users/$usernamevar
+    sudo dscl . -passwd /Users/$usernamevar $passvar
+    sudo dscl . -append /Groups/admin GroupMembership $usernamevar
+    sudo dscl . create /Users/$usernamevar Picture "/Library/User\ Pictures/Nature/Leaf.tif"
+    echo "Inital Creation Complteed"
+ 
+else
+    echo "Creating account without Admin Rights...."
+    sudo dscl . -create /Users/$usernamevar
+    sudo dscl . -create /Users/$usernamevar UserShell /bin/bash
+    sudo dscl . -create /Users/$usernamevar RealName $fullnamevar
+    sudo dscl . -create /Users/$usernamevar PrimaryGroupID 20
+    sudo dscl . -create /Users/$usernamevar NFSHomeDirectory /Users/$usernamevar
+    sudo dscl . -passwd /Users/$usernamevar $passvar
+    sudo dscl . create /Users/$usernamevar Picture  "/Library/User\ Pictures/Nature/Leaf.tif"
+    echo "Inital Creation Completed"
+    
+fi
+
+cp -R /System/Library/User\ Template/English.lproj /Users/$usernamevar
+chown -R $usernamevar:staff /Users/$usernamevar
+
+echo "Account Creation Complete $usernamevar, $fullnamevar, $newid"
+exit 0
+
+
+
+
+
+
+
