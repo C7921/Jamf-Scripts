5cf16e9e754dfd8fbbfa5a4f2ab87cdc4452139b
C7921 <c.ellemsanders@gmail.com>
2017-11-02 10:33:05 +1100

    Minor changes
    Added a better default text for own production.

--- New Computer Name.sh
+++ New Computer Name.sh
@@ -2,13 +2,13 @@
 #October 2017 
 # Prompt for Computer Name
 # Current Naming Convention for new Staff Macbook Pros MBP-Staff-ASSETTAG##
-# 
+# Feel free to change the default text. This was added for the deployment here. Leave the 2x Double Quotes for blank input.
 
  
 NewMachineName=`/usr/bin/osascript << EOT
 tell application "System Events"
     activate 
-    set NewMachineName to text returned of (display dialog "New Computer Name" default answer "")
+    set NewMachineName to text returned of (display dialog "New Computer Name" default answer "MBP-Staff-####")
 end tell
 EOT`
 
