f09aa107b0c3bd18aa4d45050a5a3cafe1531fbf
Connor Sanders <c.ellemsanders@gmail.com>
2018-02-15 11:21:41 +1100

    Added ViviInstall and ResetComputerNames
    VIvi Install - Download latest verion of vivi and silent install.
    
    Reset Computer Name  - To be used with Jamf policy to reset computer names per user basis.

---
+++ ResetComputerName.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Connor Sanders 14/2/18
+# Resets Computer Name to variable of asset Tag Number
+# Variable to be set in JSS Policy, ensure that variable is set. Option for hardcoding.
+# 
+#
+# HARDCODED VALUE FOR "NewMachineName" IS SET HERE
+NewMachineName=""
+
+# CHECK TO SEE IF A VALUE WAS PASSED IN PARAMETER 4 AND, IF SO, ASSIGN TO "NewMachineName"
+if [ "$4" != "" ] && [ "$NewMachineName" == "" ]; then
+	NewMachineName=$4
+fi
+
+if [ "$NewMachineName" == "" ]; then
+	echo "Error:  The parameter 'NewMachineName' is blank.  Please specify a value."
+	exit 1
+# Setting New Computer Name
+else
+	echo $NewMachineName
+		scutil --set HostName $NewMachineName
+		scutil --set LocalHostName $NewMachineName
+		scutil --set ComputerName $NewMachineName
+	echo "Renaming Completed!"
+# Run Recon    # Comment out/remove if not being used with Jamf Pro
+	# sudo jamf recon
+fi
+	exit 0
---
+++ ViviInstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Connor Sanders 14/2/18
+# Download and Installs the Latest Version of Vivi from Vivi Site
+# Steps -  Silent download and install latest Vivi.pkg. Removes Tmp files.
+
+url='https://api.vivi.io/mac-pkg'
+
+# Installs to TMP Directory
+	echo "Downloading Vivi pkg"
+curl -s -L -o '/tmp/Vivi.pkg' "$url"
+	echo "Download complete. Installing"
+# Installs from TMP Directory
+sudo installer -pkg /tmp/Vivi.pkg -target /
+	echo "Install complete. Removing Tmp File"
+sleep 1
+rm /tmp/Vivi.pkg
+	echo "Update Complete."
+exit 0
