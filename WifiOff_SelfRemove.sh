25b2f3a21ca0c523036d6fa8d30560eeaf512b81
C7921 <c.ellemsanders@gmail.com>
2018-02-16 11:21:40 +1100

    Updated description
    Minor changes to text syntax and layout

--- WifiOff_SelfRemove.sh
+++ WifiOff_SelfRemove.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 # Jan 2018 Connor Sanders
-# This script is simple, it gets the current Wi-Fi Device, 
-# turns if off and then removes itself.
-
-
-
+# Script gets current Wi-Fi device and turns it off. 
+# Script then deletes itself.
 
 # Get Current Wifi Device
 CURRENT_DEVICE=$(networksetup -listallhardwareports | awk '$3=="Wi-Fi" {getline; print $2}')
-
 
 # Turn off WiFi
 echo "Current Wi-Fi Device = '$CURRENT_DEVICE'"
 echo "wifi off"
 networksetup -setairportpower $CURRENT_DEVICE off
 sleep 2
-
 
 rm -- $0
 
