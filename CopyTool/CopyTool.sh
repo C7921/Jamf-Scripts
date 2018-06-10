0c1e1f7a46a62bb84e8995b1964e27964c810c90
Connor Sanders <c.ellemsanders@gmail.com>
2018-06-10 10:55:36 +1000

    Updates Goals and To Do
    MacCopy script tool work

--- CopyTool/CopyTool.sh
+++ CopyTool/CopyTool.sh
@@ -2,3 +2,18 @@
 # The start of the new and improved Mac Copy Tool
 # Created as a test for more complex functions, source scripts and other stuff
 # Connor Sanders May 2018.
+
+# GOALS
+# - Single file only. No dependencies aside from the obvious (intel mac etc).
+# - If other files or things are required, ability to download install, without needing to rerun.
+# - Clear and easy commands
+# - Clear and easy method for selecting source and destination locations
+
+# NOTES
+# - Yes, this can be made really easy using Automator.
+# - This will probably become a command line tool for mac.
+ 
+# To Do:
+# - Functions/ inital check for intel mac etc -- System Requirements
+# - Notate the steps required and assign dependencies
+# - Easy to read ReadME
