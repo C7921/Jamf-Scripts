5ffc1256f4569417b2811b59c851e2c283e62a2e
Connor Sanders <c.ellemsanders@gmail.com>
2018-06-13 08:24:46 +1000

    Working with bugs
    Issues with escaping white spaces in the arrays.

--- GoogleDriveFSReinstall.sh
+++ GoogleDriveFSReinstall.sh
@@ -15,60 +15,60 @@ driveFiles=("/Applications/Google\ Drive\ File\ Stream.app" "~/Library/Applicati
 # echo $driveFiles2
 
 # Install Utils
-# dmgName="GoogleDriveFileStream.dmg"
-# VolumeName="Install Google Drive File Stream"
-# targetDrive="/Volumes/Macintosh HD"
-# downloadURL="https://dl.google.com/drive-file-stream/GoogleDriveFileStream.dmg"
-#
-# # Function Start - Reinstall
-# function reinstallDriveFS {
-# 		echo "Downloading DMG File..."
-# 			curl -s -L -o "/tmp/$dmgName" "$downloadURL"
-# 		echo "Mounting DMG FIle..."
-# 			hdiutil mount /tmp/$dmgName -nobrowse -quiet
-# 		echo "Installing DMG..."
-# 			sudo installer -pkg /Volumes/"${VolumeName}"/GoogleDriveFileStream.pkg -target "$targetDrive"
-# 		echo "Unmounting DMG..."
-# 			hdiutil unmount /Volumes/"${VolumeName}"
-# 			sleep 5
-# 		echo "Deleting DMG File."
-# 			rm /tmp/$dmgName
-# 		echo "Install Complete."
-# }
-# # Function End - Reinstall
-#
-# # # Checks if installed
-# if [ "$appInstalled" == "$appName" ]; then
-# 		echo "${appName} is installed."
-# 		echo "Starting removing."
-#
+dmgName="GoogleDriveFileStream.dmg"
+VolumeName="Install Google Drive File Stream"
+targetDrive="/Volumes/Macintosh HD"
+downloadURL="https://dl.google.com/drive-file-stream/GoogleDriveFileStream.dmg"
+
+# Function Start - Reinstall
+function reinstallDriveFS {
+		echo "Downloading DMG File..."
+			curl -s -L -o "/tmp/$dmgName" "$downloadURL"
+		echo "Mounting DMG FIle..."
+			hdiutil mount /tmp/$dmgName -nobrowse -quiet
+		echo "Installing DMG..."
+			sudo installer -pkg /Volumes/"${VolumeName}"/GoogleDriveFileStream.pkg -target "$targetDrive"
+		echo "Unmounting DMG..."
+			hdiutil unmount /Volumes/"${VolumeName}"
+			sleep 5
+		echo "Deleting DMG File."
+			rm /tmp/$dmgName
+		echo "Install Complete."
+}
+# Function End - Reinstall
+
+# Checks if installed
+if [ "$appInstalled" == "$appName" ]; then
+		echo "${appName} is installed."
+		echo "Starting removing."
+
 		for d in "${driveFiles[@]}"
 			do
 				rm -rf $d
 				echo "$d is removed."
 			done
 		echo "Removal Completed."
-# 		#Poses reinstall
-# 		read -p "Reinstall? (Y Or N)." -n 1 -r
-# 		echo
-# 		if [[ $REPLY =~ ^[Yy]$ ]]
-# 		then
-# 			echo "Confirmed Reinstall."
-# 			reinstallDriveFS
-# 		else
-# 			echo "Reinstall Declined."
-# 		fi
-#
-# 	else
-# 		echo "${appName} not installed."
-# 		#Poses reinstall
-# 		read -p "Reinstall? (Y Or N)." -n 1 -r
-# 		echo
-# 		if [[ $REPLY =~ ^[Yy]$ ]]
-# 		then
-# 			echo "Confirmed Reinstall."
-# 			reinstallDriveFS
-# 		else
-# 			echo "Reinstall Declined."
-# 	fi
-# fi
+		#Poses reinstall
+		read -p "Reinstall? (Y Or N)." -n 1 -r
+		echo
+		if [[ $REPLY =~ ^[Yy]$ ]]
+		then
+			echo "Confirmed Reinstall."
+			reinstallDriveFS
+		else
+			echo "Reinstall Declined."
+		fi
+
+	else
+		echo "${appName} not installed."
+		#Poses reinstall
+		read -p "Reinstall? (Y Or N)." -n 1 -r
+		echo
+		if [[ $REPLY =~ ^[Yy]$ ]]
+		then
+			echo "Confirmed Reinstall."
+			reinstallDriveFS
+		else
+			echo "Reinstall Declined."
+	fi
+fi
