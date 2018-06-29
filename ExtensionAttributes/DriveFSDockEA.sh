9140cb283ec756196e3bd62c5025a44832f46654
Connor Sanders <c.ellemsanders@gmail.com>
2018-06-29 12:23:23 +1000

    Added Dock check for DriveFS
    Google Drive File Stream can now be checked to see if its on the dock.

---
+++ ExtensionAttributes/DriveFSDockEA.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Checks is DriveFS is on the Dock
+
+driveDock=$(defaults read ~/Library/Preferences/com.apple.dock.plist persistent-apps | grep -aRi -C 1 "Google Drive File Stream" | awk '{print $5, $6}' | cut -c 2-14 | grep -i "Google")
+
+if [ "$driveDock" != "" ]; then
+  Docked=True
+else
+  Docked=False
+fi
+echo "<result>$Docked</result>"
