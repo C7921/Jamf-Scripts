84d1c3b77e715c36c664f5ebd0eb7249b13466c7
Connor Sanders <c.ellemsanders@gmail.com>
2018-08-15 08:31:31 +1000

    Used in Provision Working
    Thanks to Ubcoits on the Jamf Nation forums for this easy fix. Current bug with QuickAdd PKG not creating symlinks.

---
+++ Provision/Symlink_HotFix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Thanks to Jamf Nation User ubcoits
+# https://www.jamf.com/jamf-nation/discussions/28097/jamf-pro-10-4-and-quick-add-package-bug#responseChild167370
+
+
+# check for SymLink
+if [ -L "/usr/local/bin" ]; then
+  rm "/usr/local/bin"
+  mkdir "/usr/local/bin"
+  ln -s "usr/local/jamf/bin/jamf" "/usr/local/bin/jamf"
+  ln -s "/usr/local/jamf/bin/jamfAgent" "/usr/local/bin/jamfAgent"
+  chown -R root:wheel "/usr/local/bin"
+fi
