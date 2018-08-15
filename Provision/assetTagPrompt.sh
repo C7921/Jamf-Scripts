9ab710d35740847f9f9376515169fd2147dc1ef4
Connor Sanders <c.ellemsanders@gmail.com>
2018-08-15 12:11:46 +1000

    Spacing

--- Provision/assetTagPrompt.sh
+++ Provision/assetTagPrompt.sh
@@ -3,6 +3,4 @@
 
 tag=$(osascript -e 'Tell application "System Events" to display dialog "Please enter the Asset Tag of the Computer." default answer ""' -e 'text returned of result')
 echo $tag
-
-
 sudo jamf recon -assetTag $tag
