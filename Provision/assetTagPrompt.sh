8d5669161c8ac1bf53edd184bac848094080c3ab
Connor Sanders <c.ellemsanders@gmail.com>
2018-08-21 20:07:45 +1000

    Spacing
    Better spacing and text wrap for the 'tag' variable

--- Provision/assetTagPrompt.sh
+++ Provision/assetTagPrompt.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Prompt for Asset Tag
 
-tag=$(osascript -e 'Tell application "System Events" to display dialog "Please enter the Asset Tag of the Computer." default answer ""' -e 'text returned of result')
+tag=$(
+	osascript -e 'Tell application "System Events" to display dialog "Please enter the Asset Tag of the Computer." default answer ""' -e 'text returned of result'
+	)
 echo $tag
 sudo jamf recon -assetTag $tag
