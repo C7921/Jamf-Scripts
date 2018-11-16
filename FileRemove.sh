2b14b6c6a5e36b20227bc92591c11107164ed71a
C7921 <c.ellemsanders@gmail.com>
2018-11-16 12:05:36 +1100

    One extra file location added

--- FileRemove.sh
+++ FileRemove.sh
@@ -7,6 +7,7 @@ fileLocation5="/Users/USERNAME/Desktop/file5.txt"
 fileLocation6="/Users/USERNAME/Desktop/file6.txt"
 fileLocation7="/Users/USERNAME/Desktop/file7.txt"
 fileLocation8="/Users/USERNAME/Desktop/file8.txt"
+fileLocation9="/Users/USERNAME/Desktio/file9.txt"
 
 # Checks to see if a value has been set in Parameter 4 and if so, assign to fileLocation
 # Requires at least Parameter 4 to have value set.
@@ -24,5 +25,6 @@ echo $fileLocation5 && rm -rf $fileLocation5
 echo $fileLocation6 && rm -rf $fileLocation6
 echo $fileLocation7 && rm -rf $fileLocation7
 echo $fileLocation8 && rm -rf $fileLocation8
+echo $fileLocation0 && rm -rf $fileLocation9
 
 echo "Files removed."
